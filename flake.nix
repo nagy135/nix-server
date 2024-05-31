c4000b2c7cc7efb25cbf6ca391c1c73de45670d5
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-05-31 16:17:18 +0200

    fix: delete nixvim for now

--- flake.nix
+++ flake.nix
@@ -6,27 +6,13 @@
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
   };
   outputs = inputs@{ self, nixpkgs, nixvim, home-manager, ... }: 
-  let
-    platform = "x86_64-linux";
-    nixvimPkg = import ./nixvim { 
-      inherit nixvim;
-      lib = nixpkgs.lib;
-      pkgs = nixpkgs.legacyPackages.${platform};
-    };
-  in {
+  {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        nixvimPkg
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;

224d2af8f87a74d8c40be50c34ef43aafa76a2ae
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-06-01 18:00:43 +0200

    fix: forgotten cleanup

--- configuration.nix
+++ configuration.nix
@@ -6,8 +6,6 @@ in
     imports = [
       ./hardware-configuration.nix
       ./networking.nix # generated at runtime by nixos-infect
-      ./credentials.nix
-
     ];
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 

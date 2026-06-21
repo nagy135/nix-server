8a3a2f167e19efaef6d4e95725b71d843abf2498
Viktor Nagy (nixos) <viktor.nagy1995@gmail.com>
2026-06-21 08:59:55 +0000

    feat: nix on droid public key

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
     ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA7XoDHd8Pi5tylzVnjKvKoM+5GzT7Pcuk2PfWOsGEu7''
     ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCnmNqABy0voX/rDdThGadpR5ZSF6NWJ2oWaGJvRJWF4H1PZHVJr/BSl/s7zQM5Tp4PH34+g8CNZAaFFP5aThGDv22cIAlZneM5t5HL0iHiN9/L9e9W3U7ySLDYdRls0QBnURUInNh8pK6IqqJTg8LDx6kfxOBhJyPtlLFhxqWtJSYTjm17B/tU8bvtslbg97Q1ck89VVX1g++2YCjOGhZv0HKp7X3F6RvTlJolYxUwvZ4qPdx2eXSWgSLAYJc7aDlJLdqEqPqA1senvcIYam+cWkxqnmEobIhmc4oDSnLO/Yf5vRANP/tw7VPgf9kxnXa7OEhbEt++Uts+FidZZC/xFnT9x2Rp8I/5MGLn52y5QPmSm3KTPSxBkFuzLA93opjOIiijov5EECZhtsWWN4z97rSeBu11OMXcbTKPTPCjOWxylDW83xFx6gl6/lmu5UirbRGqoOlzOoV2hrLy/MlOXX9lDU5LtlZShId4hbzt/lkctv7AcnE9QTSU/8651d8= infiniter@infiniter''
     ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDGB6YkzqRa0xmB3kS9mN6E9beWBsBVHduOgDM51BtR4 Viktor@Viktor-Mac.local''
+    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGm0dwd4tY0KSWNZ05glOkJ7Qu8SxLVF4wiBY+9xjuSb nix-on-droid@localhost''
   ];
 in {
   imports = [
@@ -18,6 +19,7 @@ in {
       projectPaths = [
         "/home/infiniter/services/vite-portfolio"
         "/home/infiniter/services/shift-distributor"
+	# "/home/infiniter/services/ai_image_edit"
       ];
       opencodePkg = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.opencode;
       createUser = false;

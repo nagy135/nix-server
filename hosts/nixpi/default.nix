1812061337a7c92c012196ebae30542cdedede73
Viktor Nagy (sensory) <viktor.nagy1995@gmail.com>
2026-09-22 22:25:47 +0200

    Route public file relay to manually managed Compose app on nixpi

--- hosts/nixpi/default.nix
+++ hosts/nixpi/default.nix
@@ -77,6 +77,7 @@ in {
       "pi-status"
       "speech"
       "conversation"
+      "relay"
     ];
   };
 
--- hosts/nixpi/nginx.nix
+++ hosts/nixpi/nginx.nix
@@ -6,6 +6,24 @@
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
 
+    # Started manually with Docker Compose in ~/services/file-relay.
+    virtualHosts."relay.infiniter.tech" = {
+      enableACME = true;
+      forceSSL = true;
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:13006";
+        extraConfig = ''
+          # The app enforces 200 MB per file; allow multipart overhead here.
+          client_max_body_size 201m;
+          client_body_timeout 1800s;
+          proxy_request_buffering off;
+          proxy_read_timeout 1800s;
+          proxy_send_timeout 1800s;
+        '';
+      };
+    };
+
     virtualHosts."conversation.infiniter.tech" = {
       enableACME = true;
       forceSSL = true;
